23c23deb569792dd34d18857ca269f07dcb683e1
mkrasnoperov <mikhail.krasnoperov@vsegda-da.com>
2023-08-13 18:51:30 +0300

    #1 init app

---
+++ src/test/java/com/mkr/rabbitexample/RabbitExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkr.rabbitexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
